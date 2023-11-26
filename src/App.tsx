2139352b1c450483bfee03697e85ed1b5e89ab80
divinejoshua <divineuche399@gmail.com>
2023-11-26 22:39:44 +0000

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import HomePage from "./pages/Index";
 import SendMessagePage from "./pages/Send";
 import SplashScreen from "./components/SplashScreen";
+import LoginPage from "./pages/Login";
+import PageNotFound404 from "./pages/PageNotFound404";
 
 function App() {
 
@@ -16,6 +18,9 @@ function App() {
       <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="send/:id" element={<SendMessagePage />} />
+          <Route path="login" element={<LoginPage />} />
+          {/* 404  */}
+          <Route path="*" element={<PageNotFound404/>}/>
       </Routes>
     </BrowserRouter>
   );
