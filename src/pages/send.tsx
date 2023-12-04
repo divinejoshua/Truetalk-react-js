4f5aedf77b9db5d320311bfc0c07082bcd7a7b5e
divinejoshua <divineuche399@gmail.com>
2023-12-04 16:22:33 +0000

    Added success message

--- src/pages/send.tsx
+++ src/pages/send.tsx
@@ -5,7 +5,7 @@ import firebase from '../utils/firebase';
 import {auth} from '../utils/firebase'
 import useUser from '../hooks/UseUserHook';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import IUser from '../interface/user.interface';
 import { isValidMessageText } from '../utils/stringvalidator';
 
@@ -19,6 +19,7 @@ export default function SendMessagePage() {
   const [user, loading, error] : [any, boolean, any]  = useAuthState(auth);
   const [maxLength, setmaxLength] = useState<number>(10)
   const [messageBody, setmessageBody] = useState<string>("")
+  const [messageSentSuccess, setmessageSentSuccess] = useState<boolean>(false)
 
   // Hooks
   const { getUserById, recieverUserDetails } = useUser(user)
@@ -27,6 +28,8 @@ export default function SendMessagePage() {
   // The send messge function
   const SendMessage = () =>{
     console.log("Sent")
+    setmessageBody("")
+    setmessageSentSuccess(true)
   }
 
   useEffect(() => {
@@ -42,51 +45,62 @@ export default function SendMessagePage() {
       <HeaderComponent/>
       <div className='p-3'>
 
-        {/* Message card  */}
-        <div className='message-card shadow-md border mt-5 py-6 px-4'>
-          <div className='profile-card flex'>
-              <img alt="truetalk" src={recieverUserDetails.profile_image} className='rounded-full h-8 w-8'/>
-              <div className='text-xs ml-3 text-gray-500'>
-                  <p className='font-bold text-md text-gray-800'>{recieverUserDetails.fullname}</p>
-                  <p className='mt-1'>Tell me what’s on your mind.</p>
+        {
+          !messageSentSuccess?
+
+          // The text box
+          <div>
+            <div className='message-card shadow-md border mt-5 py-6 px-4'>
+              <div className='profile-card flex'>
+                  <img alt="truetalk" src={recieverUserDetails.profile_image} className='rounded-full h-8 w-8'/>
+                  <div className='text-xs ml-3 text-gray-500'>
+                      <p className='font-bold text-md text-gray-800'>{recieverUserDetails.fullname}</p>
+                      <p className='mt-1'>Tell me what’s on your mind.</p>
+                  </div>
               </div>
+
+              <div className='message-body mt-5 font-bold text-gray-700'>
+                <textarea
+                onChange={e => setmessageBody(e.target.value)}
+                className='w-full border rounded p-3
+                focus:outline-none focus:border-default
+                focus:ring-default
+                focus:ring-1
+                focus:border-100
+                transition duration-0 hover:duration-150' maxLength={maxLength} rows={4} autoFocus
+                placeholder={`Write a message to ${recieverUserDetails.fullname}...`}></textarea>
+                <p className='text-xs text-gray-500 mt-4 float-right'>{messageBody.length} / {maxLength}</p>
+              </div>
+              <img alt="truetalk" src={require("../assets/header.png")} width={60} className='mt-8 opacity-50' />
+            </div>
+
+            <center>
+                <button
+                className='btn flex place-content-center mt-10 bg-default text-white px-20 py-2 rounded-full font-bold drop-shadow'
+                disabled={!recieverUserDetails?.id || !isValidMessageText(messageBody, maxLength)}
+                onClick={()=> SendMessage() }
+                >
+                  Send message
+                </button>
+            </center>
           </div>
 
-          <div className='message-body mt-5 font-bold text-gray-700'>
-            <textarea
-            onChange={e => setmessageBody(e.target.value)}
-            className='w-full border rounded p-3
-            focus:outline-none focus:border-default
-            focus:ring-default
-            focus:ring-1
-            focus:border-100
-            transition duration-0 hover:duration-150' maxLength={maxLength} rows={4} autoFocus
-            placeholder={`Write a message to ${recieverUserDetails.fullname}...`}></textarea>
-            <p className='text-xs text-gray-500 mt-4 float-right'>{messageBody.length} / {maxLength}</p>
+          :
+
+          // Display success message
+          <div className='mt-20'>
+            <center>
+              <h2 className='text-lg mb-3 text-green-700'>Message sent successfully!!!</h2>
+              <p className='font-bold text-gray-500'>Get your own link and receive messages from your friends!</p>
+              <Link to="/login">
+                <button className='btn flex place-content-center mt-7 bg-default text-white px-20 py-2 rounded-full font-bold drop-shadow'>
+                  Get yours
+                </button>
+              </Link>
+            </center>
           </div>
-          <img alt="truetalk" src={require("../assets/header.png")} width={60} className='mt-8 opacity-50' />
-        </div>
 
-        <center>
-            <button 
-            className='btn flex place-content-center mt-10 bg-default text-white px-20 py-2 rounded-full font-bold drop-shadow'
-            disabled={!recieverUserDetails?.id || !isValidMessageText(messageBody, maxLength)}
-            onClick={()=> SendMessage() }
-            >
-              Send message
-            </button>
-        </center>
-
-
-        {/* After message is sent */}
-        <div className='mt-20'>
-          <center>
-            <p className='font-bold text-gray-500'>Get your own link and receive messages from your friends!</p>
-            <button className='btn flex place-content-center mt-7 bg-default text-white px-20 py-2 rounded-full font-bold drop-shadow'>
-              Get yours
-            </button>
-          </center>
-        </div>
+        }
 
       </div>
 
